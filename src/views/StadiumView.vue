db2cbabb136bd01654e0f239186526f2c0709be1
Rubén Garzón <rubengarzonzafra@gmail.com>
2022-11-21 14:00:46 +0100

    estadios añadidos

--- src/views/StadiumView.vue
+++ src/views/StadiumView.vue
@@ -1,16 +1,233 @@
 <!-- eslint-disable prettier/prettier -->
 <template>
-  <div>
-    <p>Proximamente...</p>
+  <div class="block-stadiums">
+    <h2>Estadios</h2>
+    <img src="https://s1.eestatic.com/2022/04/02/deportes/otros-deportes/661944139_223211490_1706x960.jpg"
+      alt="estadio" />
+    <h3>
+      Estadio Al Bayt
+    </h3>
+    <p>Ubicado en Al Khor, ciudad conocida por sus actividades de pesca y también de búsqueda de perlas bajo el mar, se
+      encuentra el Estadio de Al Bait. Este se inspira en las jaimas, conocidas como bayt al sha’ar, que los nómadas de
+      Qatar
+      han utilizado durante su historia. Es el campo más alejado de Doha, aunque se encuentra a tan solo 40 minutos en
+      coche.</p>
+    <img src="https://s1.eestatic.com/2022/11/16/deportes/futbol/mundial/718938802_228756616_1706x960.jpg"
+      alt="albayt" />
+    <p>Tiene una capacidad para 60.000 espectadores y también techo retráctil, además de estar rodeado por 400.000
+      metros
+      cuadrados de espacios verdes y áreas de juego que aportan un toque de sostenibilidad. En él se disputa el partido
+      inaugural del Mundial de Qatar, así como cinco partidos más de la fase de grupos, entre ellos el esperado España -
+      Alemania, además uno de octavos, uno de cuartos y una de las semifinales.
+    </p>
+    <img src="https://s1.eestatic.com/2022/11/16/deportes/futbol/mundial/718938807_228756763_1706x960.jpg"
+      alt="albayt2" />
+    <h3>Estadio Al Yanub</h3>
+    <p>En la ciudad meridional de Al Wakrah se encuentra el Estadio Al Yanub, cuyo diseño recuerda a los dhow. Estos son
+      un
+      tipo de embarcaciones utilizadas en la pesca de perlas. Todo un símbolo en la región de Qatar. Precisamente, Al
+      Wakhrah
+      es conocida por ser uno de los asentamientos humanos más antiguos del país. El campo dispone de techo retráctil y
+      alimenta el sistema de refrigeración con energía solar.</p>
+    <img src="https://s1.eestatic.com/2022/11/16/deportes/futbol/mundial/718938827_228757378_1706x960.jpg"
+      alt="alyanub" />
+    <p>Con capacidad para 40.000 aficionados en sus gradas, 20.000 de estos asientos serán retirados después de la Copa
+      del
+      Mundo de Qatar 2022 y se donarán a proyectos de desarrollo futbolístico en el extranjero. En él se celebran seis
+      partidos de la fase de grupos y también uno de los encuentros correspondientes a los octavos de final.</p>
+    <img src="https://s1.eestatic.com/2022/11/16/deportes/futbol/mundial/718938829_228757436_1706x960.jpg"
+      alt="alyanub2" />
+    <h3>Estadio Áhmad bin Ali</h3>
+    <p>A orillas del desierto se encuentra el Estadio Áhmad bin Ali, el cual conecta con la ciudad de Al Rayyan gracias
+      a una
+      nueva línea de tren. Destaca por su fachada brillante, compuesta por motivos característicos del país: la
+      importancia de
+      la familia, la belleza del desierto, la flora y la fauna autóctonas y el comercio local e internacional.</p>
+    <img src="https://s1.eestatic.com/2022/11/16/deportes/futbol/mundial/718938837_228757674_1706x960.jpg"
+      alt="ahmad" />
+    <p>Con un aforo para 40.000 aficionados, el Estadio Áhmad bil Ali se levantó en los terreno de un antiguo campo de
+      fútbol
+      para este Mundial de Qatar 2022. La capacidad de este se verá reducida a la mitad tras el campeonato para
+      adaptarlo a
+      las necesidades de los ciudadanos. Dentro de él hay un centro comercial. En él se celebran seis partidos de la
+      fase de
+      grupos y uno de octavos.</p>
+    <img src="https://s1.eestatic.com/2022/11/16/deportes/futbol/mundial/718938838_228757703_1706x960.jpg"
+      alt="ahmad2" />
+    <h3>Estadio Al Thumama</h3>
+    <p>Localicado a 12 kilómetros al sur de Doha, cerca del aeropuerto internacional de Hamad, el nombre de este estadio
+      es el
+      de un árbol nativo de Qatar y el diseño se basa en el gahfiya, un sombrero árabe. Se trata de un elemento central
+      en las
+      familias árabes que simboliza la dignidad e independencia que incorpora cada joven en su camino a la edad adulta.
+      Su
+      arquitectura también busca celebrar la cultura y las tradiciones árabes.</p>
+    <img src="https://s1.eestatic.com/2022/11/16/deportes/futbol/mundial/718938848_228758025_1706x960.jpg"
+      alt="althumama" />
+    <p>Con capacidad para 40.000 espectadores, su aforo se verá reducido a 20.000 localidades. El resto se donará a
+      países en
+      desarrollo. En la parte superior del Estadio Al Thumama se instalará un hotel boutique y también se abrirá una
+      filial de
+      la Clínica Deoprtiva Aspetar. En él jugará España ante Costa Rica, además de otros cinco partidos de la fase de
+      grupos,
+      uno de octavos y uno de cuartos de final.</p>
+    <img src="https://s1.eestatic.com/2022/11/16/deportes/futbol/mundial/718938847_228757996_1706x960.jpg"
+      alt="althumama2" />
+    <h3>Education City Stadium</h3>
+    <p>El Estadio de la Educación o Education City Stadium está ubicado, como su nombre indica, en la Ciudad de la
+      Educación,
+      sede de la Qatar Foundation. Su fachada está adornada por triángulos que forman complejos patrones geométricos en
+      forma
+      de diamante, los cuales parecen cambiar de color cuando incide el sol. Conocido también como el Diamante del
+      Desierto,
+      fue la primera sede del Mundial calificada con cinco estrellas.</p>
+    <img src="https://s1.eestatic.com/2022/11/16/deportes/futbol/mundial/718938864_228758508_1706x960.jpg"
+      alt="educationcity" />
+    <p>Al igual que otros estadios del Mundial de Qatar, el aforo del Estadio de la Educación se reducirá después de la
+      cita
+      más importante del fútbol. De 40.000 pasará a 25.000, siendo esos 15.000 asientos restantes donados a países en
+      desarrollo. Apenas a 7 kilómetros de distancia, los aficionados que quieran pueden encontrar un campo de golf y
+      centros
+      comerciones. En este campo se juegan seis partidos de la fase de grupos, uno de octavos de final y uno de cuartos.
+    </p>
+    <img src="https://s1.eestatic.com/2022/11/16/deportes/futbol/mundial/718938863_228758479_1706x960.jpg"
+      alt="educationcity2" />
+    <h3>Estadio Khalifa International</h3>
+    <p>De los ocho estadios en los que se celebran los distintos partidos del Mundial de Qatar 2022, el Khalifa
+      International
+      es el único que ya existía antes. Fue construido en 1976 y se remodeló en 2017. Ubicado en el centro de todas las
+      sedes
+      en Al Rayyan, en el pasado albergó el Campeonato Mundial de Atletismo y la final del Mundial de Clubes de la FIFA
+      del
+      año 2019 . Son característicos los dos arcos que sostienen sus techos y que coronan su estructura simbolizando la
+      unión
+      de los aficionados de todo el planeta.</p>
+    <img src="https://s1.eestatic.com/2022/11/16/deportes/futbol/mundial/718938869_228758656_1706x960.jpg"
+      alt="khalifa" />
+    <p>Su aforo es de 40.000 localidades y no se modificará después de la Copa del Mundo. Ubicado a 10 kilómetros del
+      centro de
+      la ciudad, forma parte de un complejo todavía más amplio en el que se pueden encontrar el centro acuático olímpico
+      de
+      Qatar, así como un auditorio techado, el centro comercial Villaggio Mall, un parque y el hotel The Torch Doha.
+      También
+      albergará el Museo Olímpico y Deportivo de Qatar 3-2-1. La selección española juega en él ante Japón, además de
+      otros
+      cinco partidos de la fase de grupos, uno de octavos y el encuentro por el tercer y cuarto puesto.</p>
+    <img src="https://s1.eestatic.com/2022/11/16/deportes/futbol/mundial/718938870_228758685_1706x960.jpg"
+      alt="khalifa2" />
+    <h3>Estadio de Lusail</h3>
+    <p>El Estadio de Lusail es el más importante de Qatar. Por esto, será en él donde el próximo 20 de diciembre se
+      conozca al
+      nuevo campeón del mundo del fútbol. Ubicado a 15 kilómetros al norte de Doha, su diseño está inspirado en la danza
+      de
+      luces y sombras de los tradicionales fanales, lo que ofrece una inmejorable representación del mundo árabe, ya que
+      también acuña motivos de vasijas, cuencos y piezas de arte de la región.</p>
+    <img src="https://s1.eestatic.com/2022/11/16/deportes/futbol/mundial/718938902_228759625_1706x960.jpg"
+      alt="lusail" />
+    <p>Con capacidad para 80.000 espectadores, se define como el estadio "donde el deporte y la cultura se encuentran".
+      Cubierto por paneles solares para crear su propia electricidad y la de las zonas aledañas, también podrá abrirse y
+      cerrarse su parte central cuando las condiciones climatológicas lo permitan. Después del Mundial, se convertirá en
+      un
+      centro comunitario de escuelas, tiendas, clínicas de salud e instalaciones deportivas bajo el techo del estadio.
+      En él
+      se disputan seis partidos de la fase de grupos, uno de octavos, uno de cuartos, una semifinal y la gran final.</p>
+    <img src="https://s1.eestatic.com/2022/11/16/deportes/futbol/mundial/718938907_228759778_1706x960.jpg"
+      alt="lusail2" />
+    <h3>Estadio 974</h3>
+    <p>El Estadio 974 o Ras Abu Aboud es el campo más peculiar de los ochos del Mundial de Qatar 2022. Cuenta con una
+      particular peculiariedad y es que es el único de los recintos que será desmontado por completo después del torneo.
+      Se
+      construyó utilizando 974 (prefijo telefónico de Qatar) contenedores de transporte marítimo estándar certificados y
+      elementos modulares de acero, en homenaje al puerto cercano y a la historia industrial de la parcela en la que se
+      asienta, cuyo resultado es un conglomerado de atrevidos colores.</p>
+    <img src="https://s1.eestatic.com/2022/11/16/deportes/futbol/mundial/718938917_228760081_1706x960.jpg" alt="974" />
+    <p>El color de los contenedores tiene significado: amarillo para los baños; azul para las zonas de venta al público;
+      verde,
+      los cuartos de seguridad y de primeros auxilios; plata para las entradas VIP y las zonas de rezos de los hombres;
+      y el
+      negro, para las zonas de rezo para las mujeres y de identificación en zonas de entradas. En el Estadio 974 se
+      disputan
+      seis partidos de la fase de grupos y uno de octavos de final.</p>
+    <img src="https://s1.eestatic.com/2022/11/16/deportes/futbol/mundial/718938918_228760110_1706x960.jpg" alt="9742" />
   </div>
 </template>
 <!-- eslint-disable prettier/prettier -->
 <style>
-div p {
+.block-stadiums {
+  background-color: white;
+  width: 50em;
+  height: 100%;
+  margin: 0 auto;
+  margin-top: 4em;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  color: black;
+}
+
+.block-stadiums h2 {
   font-size: 25px;
-  color: white;
-  text-align: center;
-  margin-top: 20px;
   font-weight: 600;
+  margin: 0;
+  padding: 0;
+  padding-left: 1em;
+  padding-top: 1em;
+  padding-bottom: 1em;
+  border-bottom: 1px solid #eaeaea;
+  text-align: center;
+}
+
+.block-stadiums h3 {
+  font-size: 20px;
+  font-weight: 600;
+  margin: 0;
+  padding: 0;
+  padding-left: 1em;
+  padding-top: 1em;
+  padding-bottom: 1em;
+  border-bottom: 1px solid #eaeaea;
+  text-align: center;
+}
+
+.block-stadiums p {
+  font-size: 15px;
+  font-weight: 400;
+  margin: 0;
+  padding: 0;
+  padding-left: 1em;
+  padding-top: 1em;
+  padding-bottom: 1em;
+  padding-right: 1em;
+  text-align: justify;
+}
+
+.block-stadiums img {
+  width: 100%;
+}
+
+@media (max-width: 768px) {
+  .block-stadiums {
+    width: 100%;
+    margin-bottom: 4em;
+  }
+
+  .block-stadiums h1 {
+    font-size: 23px;
+  }
+
+  .block-stadiums h2 {
+    font-size: 20px;
+  }
+
+  .block-stadiums h3 {
+    font-size: 15px;
+  }
+
+  .block-stadiums p {
+    font-size: 12px;
+  }
+
+  .block-stadiums img {
+    width: 100%;
+  }
 }
 </style>
